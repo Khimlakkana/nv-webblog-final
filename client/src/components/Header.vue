b565e9db44905c46c1d778ac2746ef2ff2878456
Khimlakkana <112000179+Khimlakkana@users.noreply.github.com>
2022-10-08 05:04:55 +0700

    .

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,9 +2,9 @@
 	<div>
 		<div class="nv-navbar">
 			<ul class="nav">
-				<li><router-link :to="{ name: 'blogs' }">shops</router-link></li>
+				<li><router-link :to="{ name: 'Product' }">shops</router-link></li>
 				<li><router-link :to="{ name: 'users' }">Users</router-link></li>
-				<li><router-link :to="{ name: 'comments' }">Comments</router-link></li>
+				<li><router-link :to="{ name: 'comments' }">Add Product</router-link></li>
 				<li><router-link :to="{ name: 'login' }">Login</router-link></li>
 				<li><router-link :to="{ name: 'blogs' }">Blogs</router-link></li>
 			</ul>
